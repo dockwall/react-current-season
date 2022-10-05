03fd871e886a49be339cef1f6f9f28cb875675de
Stas <leshuksu@gmail.com>
2022-10-05 22:07:39 +0300

    Refactored App to a class-based (to access the state system)

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client"
 import SeasonDisplay from "./SeasonDisplay";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+class App extends React.Component {
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        error => console.log(error)
-    );
-    return <div>Hi there!</div>;
-};
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            error => console.log(error)
+        );
+
+        return <div>Latitude: </div>
+    }
+}
 
 root.render(<App />);
