562a5328c62bd7a30ae5cc9964f93f7e28477307
Stas <leshuksu@gmail.com>
2022-10-05 20:38:15 +0300

    Used Geolocation API for determinate hemisphere of user (latitude)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import SeasonDisplay from "./SeasonDisplay";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        error => console.log(error)
+    );
     return <div>Hi there!</div>;
 };
 
