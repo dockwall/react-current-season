0162b6f3cd552c32ab925243ec35121b6569b69c
Stas <leshuksu@gmail.com>
2022-10-07 13:11:24 +0300

    Added conditionally render (including Geolocation API errors)

--- src/index.js
+++ src/index.js
@@ -6,18 +6,24 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { lat: null }
+        this.state = { lat: null, errorMessage: "" };
 
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({ lat: position.coords.latitude }),
-            error => this.setState({ lat: error.message })
+            error => this.setState({ errorMessage: error.message })
         );
     }
 
 
 
     render() {
-        return <div>Latitude: {this.state.lat}</div>
+        if (this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        } else if (this.state.errorMessage) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        return <div>Loading...</div>
     }
 }
 
