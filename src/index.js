6a3fef6a058a3dc6edf92e3a5d0892123892e1de
Stas <leshuksu@gmail.com>
2022-10-07 11:45:42 +0300

    Now user position/error by Geolocation API is rendering (played with state system)

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import SeasonDisplay from "./SeasonDisplay";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { lat: null }
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            error => this.setState({ lat: error.message })
+        );
+    }
+
+
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            error => console.log(error)
-        );
-
-        return <div>Latitude: </div>
+        return <div>Latitude: {this.state.lat}</div>
     }
 }
 
